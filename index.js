e7696caae18b1e5b345541d24dd3334664fe41eb
Sarah Ochoa <sodisenografico@gmail.com>
2024-10-19 11:18:15 -0500

    desafio Js

---
+++ index.js
@@ -0,0 +1,10 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+cantidad = document.querySelector('.cantidad')
+
+contador = Number(cantidad.innerHTML)
+
+precioTotal = document.querySelector('.valor-total')
